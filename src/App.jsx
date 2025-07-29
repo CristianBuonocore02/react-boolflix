2ed96f7e14fd31ae08626c2f7fbee82e87bce4bd
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-07-29 11:29:18 +0200

    I created our app by removing the basic content of Vite and inserted test title

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [films, setfilms] = useState()
+
+  return (
+    <>
+      <h1>prova</h1>
+    </>
+  )
+}
+
+export default App
